9264fa35ebd91627abcfe613ea00c3deb1a6f070
rahuldeundi <67316306+rahuldeundi@users.noreply.github.com>
2020-06-23 15:45:14 +0530

    First draft

---
+++ Capstone Project.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook will be mainly used for Capstone Project
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print ("Hello Capstone Project")
+
